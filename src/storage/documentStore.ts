46dea74b73ad152572dbf2c4bd5e300e484d2b34
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-04-21 16:50:12 +0530

    feat:add document store for its operations
    add Dosument store operations in the file

---
+++ src/storage/documentStore.ts
@@ -0,0 +1,45 @@
+import { getDB } from "../storage/index";
+import type { Doc } from "../types/storageTypes";
+
+export function addDocument(doc: Doc): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const tx = getDB().transaction("documents", "readwrite");
+    const store = tx.objectStore("documents");
+
+    const req = store.add(doc);
+    //if succeeds then this else error
+    req.onsuccess = () => resolve();
+    req.onerror = () => reject(req.error);
+  });
+}
+
+export function updateDocument(doc: Doc): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const tx = getDB().transaction("documents", "readwrite");
+    const store = tx.objectStore("documents");
+    const req = store.put(doc);
+    req.onsuccess = () => resolve();
+    req.onerror = () => reject(req.error);
+  });
+}
+
+export function deleteDocument(id: number): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const tx = getDB().transaction("documents", "readwrite");
+    const store = tx.objectStore("documents");
+    const req = store.delete(id);
+    req.onsuccess = () => resolve();
+    req.onerror = () => reject(req.error);
+  });
+}
+
+export function getDocument(id: number): Promise<Doc | undefined> {
+  return new Promise((resolve, reject) => {
+    const tx = getDB().transaction("documents", "readonly");
+    const store = tx.objectStore("documents");
+
+    const req = store.get(id);
+    req.onsuccess = () => resolve(req.result);
+    req.onerror = () => reject(req.error);
+  });
+}
