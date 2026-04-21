d53df517fa149913431b0a6e1a7d4276af063299
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-04-21 16:49:09 +0530

    feat:add Types and indexDb setup
    add types used in app along with Db Setup

---
+++ src/types/storageTypes.ts
@@ -0,0 +1,39 @@
+export type Doc = {
+  id: number;
+  title: string;
+  content: string;
+  updatedAt: number;
+};
+
+export const operationTypes = {
+  insert: "insert",
+  update: "update",
+  delete: "delete",
+} as const;
+
+export type OperationType =
+  (typeof operationTypes)[keyof typeof operationTypes];
+
+export type Op = {
+  opId: string;
+  docId: number;
+  userId: string;
+  type: OperationType;
+  position: number;
+  value: string;
+  timestamp: number;
+};
+
+export type Cursor = {
+  cursorId: string;   // userId or tabId
+  docId: number;
+  userId: string;
+  position: number;
+  updatedAt: number;
+};
+
+export type Tab = {
+  tabId: string;
+  docId: number;
+  lastSeen: number;
+};
