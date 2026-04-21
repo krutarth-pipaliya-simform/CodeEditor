d66c1be2187d6e839e4242827900be889b725956
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-04-21 17:37:34 +0530

    chore: remove unnecessary logs

--- src/events/toggleSidebarEvent.ts
+++ src/events/toggleSidebarEvent.ts
@@ -1,8 +1,7 @@
 export default function toggleSidebarEvent() {
-    const toggle = document.querySelector('.aside-toggle');
-    toggle?.addEventListener('click', (e) => {
-        const aside = document.querySelector('aside');
-        aside?.classList.toggle('hidden');
-        console.log(aside, toggle)
-    })
+    const toggle = document.querySelector(".aside-toggle");
+    toggle?.addEventListener("click", (e) => {
+        const aside = document.querySelector("aside");
+        aside?.classList.toggle("hidden");
+    });
 }
