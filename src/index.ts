f2ad61933c59b3f4315547bf6f3e279bc4907114
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-04-22 17:30:14 +0530

    fix:change user type to userId only

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { runTest } from "./test/index.js";
+import { startEvents } from "./events/index.js";
 
 try{
     await runTest();
+    startEvents();
 }
 catch(err)
 {
     console.error("runTest failed:", err);
 }
+
 
 
