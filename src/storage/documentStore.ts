9eb5e2497d049d941cc60b62d81aa2881700db47
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-04-22 17:08:25 +0530

    fix:change operation position to row and column instead of string
    change position type,change name of type in operations

--- src/storage/documentStore.ts
+++ src/storage/documentStore.ts
@@ -2,44 +2,44 @@ import { getDB } from "../storage/index.js";
 import type { Doc } from "../types/storageTypes.js";
 
 export function addDocument(doc: Doc): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const tx = getDB().transaction("documents", "readwrite");
-    const store = tx.objectStore("documents");
+    return new Promise((resolve, reject) => {
+        const tx = getDB().transaction("documents", "readwrite");
+        const store = tx.objectStore("documents");
 
-    const req = store.add(doc);
-    //if succeeds then this else error
-    req.onsuccess = () => resolve();
-    req.onerror = () => reject(req.error);
-  });
+        const req = store.add(doc);
+        //if succeeds then this else error
+        req.onsuccess = () => resolve();
+        req.onerror = () => reject(req.error);
+    });
 }
 
 export function updateDocument(doc: Doc): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const tx = getDB().transaction("documents", "readwrite");
-    const store = tx.objectStore("documents");
-    const req = store.put(doc);
-    req.onsuccess = () => resolve();
-    req.onerror = () => reject(req.error);
-  });
+    return new Promise((resolve, reject) => {
+        const tx = getDB().transaction("documents", "readwrite");
+        const store = tx.objectStore("documents");
+        const req = store.put(doc);
+        req.onsuccess = () => resolve();
+        req.onerror = () => reject(req.error);
+    });
 }
 
 export function deleteDocument(id: number): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const tx = getDB().transaction("documents", "readwrite");
-    const store = tx.objectStore("documents");
-    const req = store.delete(id);
-    req.onsuccess = () => resolve();
-    req.onerror = () => reject(req.error);
-  });
+    return new Promise((resolve, reject) => {
+        const tx = getDB().transaction("documents", "readwrite");
+        const store = tx.objectStore("documents");
+        const req = store.delete(id);
+        req.onsuccess = () => resolve();
+        req.onerror = () => reject(req.error);
+    });
 }
 
 export function getDocument(id: number): Promise<Doc | undefined> {
-  return new Promise((resolve, reject) => {
-    const tx = getDB().transaction("documents", "readonly");
-    const store = tx.objectStore("documents");
+    return new Promise((resolve, reject) => {
+        const tx = getDB().transaction("documents", "readonly");
+        const store = tx.objectStore("documents");
 
-    const req = store.get(id);
-    req.onsuccess = () => resolve(req.result);
-    req.onerror = () => reject(req.error);
-  });
+        const req = store.get(id);
+        req.onsuccess = () => resolve(req.result);
+        req.onerror = () => reject(req.error);
+    });
 }
--- src/types/storageTypes.ts
+++ src/types/storageTypes.ts
@@ -1,45 +1,49 @@
 export type Doc = {
-  id: number;
-  title: string;
-  content: string;
-  updatedAt: number;
+    id: number;
+    title: string;
+    content: string;
+    updatedAt: number;
 };
 
 export const operationTypes = {
-  insert: "insert",
-  update: "update",
-  delete: "delete",
+    insert: "insert",
+    update: "update",
+    delete: "delete",
 } as const;
 
 export type OperationType =
-  (typeof operationTypes)[keyof typeof operationTypes];
+    (typeof operationTypes)[keyof typeof operationTypes];
 
+export type position = {
+    row: number;
+    column: number;
+};
 export type Op = {
-  opId: string;
-  docId: number;
-  userId: string;
-  type: OperationType;
-  position: number;
-  value: string;
-  timestamp: number;
+    operationId: string;
+    documentId: number;
+    userId: string;
+    type: OperationType;
+    position: position;
+    value: string;
+    timestamp: number;
 };
 
 export type Cursor = {
-  cursorId: string;   // userId or tabId
-  docId: number;
-  userId: string;
-  position: number;
-  updatedAt: number;
+    cursorId: string; // userId or tabId
+    docId: number;
+    userId: string;
+    position: number;
+    updatedAt: number;
 };
 
 export type Tab = {
-  tabId: string;
-  docId: number;
-  lastSeen: number;
+    tabId: string;
+    docId: number;
+    lastSeen: number;
 };
 
 export type User = {
-  userId: string;
-  username: string;
-  createdAt: number;
+    userId: string;
+    username: string;
+    createdAt: number;
 };
