1cd1eb3166d6c2e33d7d7ad4c5bf9e32a3b4f401
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-05-11 14:56:32 +0530

    fix: remove line number feature

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,6 +1,5 @@
 import endSearchButtonOnClick from "./endSearchButtonOnClick.js";
 import keyboardEvents from "./keyboardEvents.js";
-import lineNumberChangeEvent from "./lineNumberChangeEvent.js";
 import searchButtonOnClick from "./searchButtonOnClick.js";
 import toggleSidebarEvent from "./toggleSidebarEvent.js";
 
@@ -8,6 +7,5 @@ export function startEvents() {
     toggleSidebarEvent();
     searchButtonOnClick();
     endSearchButtonOnClick();
-    lineNumberChangeEvent();
     keyboardEvents();
 }
