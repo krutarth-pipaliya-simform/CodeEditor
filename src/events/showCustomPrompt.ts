0fefe0e879d8d950b0f5855dfef266c2175a4724
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-05-11 19:30:25 +0530

    fix:change the prompt to dialog

--- src/events/showCustomPrompt.ts
+++ src/events/showCustomPrompt.ts
@@ -2,18 +2,18 @@ import { generateUser } from "./generateUser.js";
 
 export function showCustomPrompt() {
     document.addEventListener("DOMContentLoaded", () => {
-        const overlay = <HTMLDivElement>document.getElementById("customPrompt");
+        const dialog = <HTMLDialogElement>document.getElementById("customPrompt");
         const input = <HTMLInputElement>document.getElementById("promptInput");
         const okBtn = <HTMLButtonElement>document.getElementById("ok-btn");
 
-        // Show prompt on page load
-        overlay.style.display = "flex";
+        // Open dialog
+        dialog.showModal();
 
         // OK button click
         okBtn.addEventListener("click", () => {
             const value = input.value.trim();
             if (value) {
-                overlay.style.display = "none";
+                dialog.close();
                 generateUser(value);
             } else {
                 alert("Please enter a valid name.");
