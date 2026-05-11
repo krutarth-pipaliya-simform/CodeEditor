c2d445f524f811dd80a624f2a61b6be4dae4901a
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-05-11 19:10:21 +0530

    fix:change the asssertion to generics

--- src/events/showCustomPrompt.ts
+++ src/events/showCustomPrompt.ts
@@ -1,23 +1,23 @@
 import { generateUser } from "./generateUser.js";
 
 export function showCustomPrompt() {
-  document.addEventListener("DOMContentLoaded", () => {
-    const overlay = document.getElementById("customPrompt") as HTMLDivElement;
-    const input = document.getElementById("promptInput") as HTMLInputElement;
-    const okBtn = document.getElementById("ok-btn") as HTMLButtonElement;
+    document.addEventListener("DOMContentLoaded", () => {
+        const overlay = <HTMLDivElement>document.getElementById("customPrompt");
+        const input = <HTMLInputElement>document.getElementById("promptInput");
+        const okBtn = <HTMLButtonElement>document.getElementById("ok-btn");
 
-    // Show prompt on page load
-    overlay.style.display = "flex";
+        // Show prompt on page load
+        overlay.style.display = "flex";
 
-    // OK button click
-    okBtn.addEventListener("click", () => {
-      const value = input.value.trim();
-      if (value) {
-        overlay.style.display = "none";
-        generateUser(value)
-      } else {
-        alert("Please enter a valid name.");
-      }
+        // OK button click
+        okBtn.addEventListener("click", () => {
+            const value = input.value.trim();
+            if (value) {
+                overlay.style.display = "none";
+                generateUser(value);
+            } else {
+                alert("Please enter a valid name.");
+            }
+        });
     });
-  });
 }
