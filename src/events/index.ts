5bc430829ca96fc238c62daba319471262a8088b
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-05-12 11:10:19 +0530

    fix:change user Initials filename

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,7 +2,7 @@ import endSearchButtonOnClick from "./endSearchButtonOnClick.js";
 import lineNumberChangeEvent from "./lineNumberChangeEvent.js"
 import searchButtonOnClick from "./searchButtonOnClick.js";
 import { showCustomPrompt } from "./showCustomPrompt.js";
-import { setUserInitials } from "./showuserInitials.js";
+import { setUserInitials } from "./showUserInitials.js";
 import toggleSidebarEvent from "./toggleSidebarEvent.js";
 
 export function startEvents() {
