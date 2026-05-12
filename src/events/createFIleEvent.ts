160a7f0c519c6b04d702fe105bb64afbe6a90f14
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-05-12 15:04:00 +0530

    fix: log removed

--- src/events/createFIleEvent.ts
+++ src/events/createFIleEvent.ts
@@ -2,7 +2,6 @@ export function createFileEvent() {
     const createButton = document.querySelector(".create-file-button");
     if (!(createButton instanceof HTMLButtonElement)) return;
     createButton.addEventListener("click", async () => {
-        console.log("called");
         const prompt = document.getElementById("custom-prompt");
         if (!(prompt instanceof HTMLDivElement)) return;
         prompt.classList.remove("hidden");
