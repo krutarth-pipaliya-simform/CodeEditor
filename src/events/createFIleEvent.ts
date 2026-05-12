55c15223d9b5912708d757fe2b6d6b8bdc3463bc
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-05-12 12:42:17 +0530

    add create file functionality

---
+++ src/events/createFIleEvent.ts
@@ -0,0 +1,10 @@
+export function createFileEvent() {
+    const createButton = document.querySelector(".create-file-button");
+    if (!(createButton instanceof HTMLButtonElement)) return;
+    createButton.addEventListener("click", async () => {
+        console.log("called");
+        const prompt = document.getElementById("custom-prompt");
+        if (!(prompt instanceof HTMLDivElement)) return;
+        prompt.classList.remove("hidden");
+    });
+}
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,7 +1,9 @@
 import { createBroadcast } from "./broadCast.js";
+import { createFileEvent } from "./createFIleEvent.js";
 import endSearchButtonOnClick from "./endSearchButtonOnClick.js";
 import keyboardEvents from "./keyboardEvents.js";
 import { mouseMoveEvent } from "./mouseMoveEvent.js";
+import { promptButtonEvent } from "./promptButtonEvent.js";
 import searchButtonOnClick from "./searchButtonOnClick.js";
 import toggleSidebarEvent from "./toggleSidebarEvent.js";
 
@@ -12,4 +14,6 @@ export function startEvents() {
     keyboardEvents();
     createBroadcast();
     mouseMoveEvent();
+    createFileEvent();
+    promptButtonEvent();
 }
