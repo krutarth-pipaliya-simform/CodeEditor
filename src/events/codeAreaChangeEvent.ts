648d7031292b81dbb867722ce875a379776ea44d
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-05-12 16:29:26 +0530

    refact: remove logs

--- src/events/codeAreaChangeEvent.ts
+++ src/events/codeAreaChangeEvent.ts
@@ -4,9 +4,7 @@ import { getDocument, updateDocument } from "../storage/documentStore.js";
 export function codeAreaChangeEvent() {
     const codeArea = document.querySelector(".code-area");
     if (!(codeArea instanceof HTMLDivElement)) return;
-            console.log('hi');
     codeArea.addEventListener("input", async (e) => {
-        console.log(e)
         const currentDocument = await getDocument(getCurrentFile());
         if (!currentDocument) return;
         currentDocument.content = codeArea.textContent;
