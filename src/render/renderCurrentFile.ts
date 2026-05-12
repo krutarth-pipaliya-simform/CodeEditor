67d734562edeb9f10e115007d77cf059c5b5dc12
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-05-12 14:45:36 +0530

    feat: add currentfile rendering function

---
+++ src/render/renderCurrentFile.ts
@@ -0,0 +1,19 @@
+import { getDocument } from "../storage/documentStore.js";
+
+let currentFile = "";
+
+export async function renderCurrentFile() {
+    const codeArea = document.querySelector(".code-area");
+    if (!(codeArea instanceof HTMLDivElement)) return;
+    const currentDocument = await getDocument(getCurrentFile());
+    if (!currentDocument) return;
+    codeArea.innerHTML = currentDocument.content;
+}
+
+export function getCurrentFile() {
+    return currentFile;
+}
+
+export function setCurrentFile(fileName: string) {
+    currentFile = fileName;
+}
--- src/render/renderFiles.ts
+++ src/render/renderFiles.ts
@@ -1,11 +1,13 @@
 import { createLi } from "../events/promptButtonEvent.js";
 import { getAllDocuments } from "../storage/documentStore.js";
+import { getCurrentFile, setCurrentFile } from "./renderCurrentFile.js";
 
 export async function renderFiles() {
     const list = document.querySelector(".file-list");
     const res = await getAllDocuments();
     if (!res) return;
     res.forEach((ele) => {
+        if (!getCurrentFile()) setCurrentFile(ele.title);
         const li = createLi(ele.title);
         list?.appendChild(li);
     });
