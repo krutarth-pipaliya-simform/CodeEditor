d0c203ec8b7919b3ee33b5d2b7e919411318e363
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-05-13 11:55:53 +0530

    refactor:change the highlight functionality for textarea

--- src/events/highlighChangeEvent.ts
+++
@@ -1,5 +0,0 @@
-import { removeHighlight } from "./searchButtonOnClick.js";
-
-export default function highlightChangeEvent(span: HTMLSpanElement) {
-    if (span.dataset.text !== span.innerHTML) removeHighlight(span);
-}
